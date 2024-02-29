34f5e2173c0c2fd755bf267c515a07bee279b910
Poon <ivanholin479@gmail.com>
2024-03-01 02:18:18 +0500

    Jump

--- Structures/Main.py
+++ Structures/Main.py
@@ -9,112 +9,185 @@ big_long_list = list(range(10_000_000))
 duplicated_list = [item for item in long_list for _ in range(2)]  # Добавление каждого элемента дважды
 unsorted_list = random.sample(range(1000), 1000)
 
+print("Linear search:")
 # Линейный поиск со списком из 10 элементов
+print("Short list:")
 start_time = time.perf_counter_ns()
 res = linear_search(short_list, 7)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Linear search time on a short list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Линейный поиск со списком из 10_000 элементов
+print("Long list:")
 start_time = time.perf_counter_ns()
 res = linear_search(long_list, 777)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Linear search time on a long list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Линейный поиск со списком из 10_000_000 элементов
+print("Super long list:")
 start_time = time.perf_counter_ns()
 res = linear_search(big_long_list, 777777)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Linear search time on a super long list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Линейный поиск со списком из 10_000 элементов с повторениями
+print("Duplicated list:")
 start_time = time.perf_counter_ns()
 res = linear_search(duplicated_list, 7)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Linear search time on a duplicated list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Линейный поиск с неотсортированным списком из 10_000 элементов с повторениями
+print("Unsorted duplicated list:")
 start_time = time.perf_counter_ns()
 res = linear_search(unsorted_list, 7)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Linear search time on a unsorted duplicated list: ", execution_time)
+print("\tTime: ", execution_time)
 
+print("\nBinary search:")
 # Бинарный поиск со списком из 10 элементов
+print("Short list:")
 start_time = time.perf_counter_ns()
 res = binary_search(short_list, 7)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Binary search time on a short list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Бинарный поиск со списком из 10_000 элементов
+print("Long list:")
 start_time = time.perf_counter_ns()
 res = binary_search(long_list, 777)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Binary search time on a long list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Бинарный поиск со списком из 10_000_000 элементов
+print("Super long list:")
 start_time = time.perf_counter_ns()
 res = binary_search(big_long_list, 777777)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Binary search time on a super long list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Бинарный поиск со списком из 10_000 элементов с повторениями
+print("Duplicated list:")
 start_time = time.perf_counter_ns()
 res = binary_search(duplicated_list, 777)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Binary search time on a duplicated list: ", execution_time)
+print("\tTime: ", execution_time)
 
 # Бинарный поиск с неотсортированным списком из 10_000 элементов с повторениями
+print("Unsorted duplicated list:")
 start_time = time.perf_counter_ns()
 res = binary_search(unsorted_list, 777)
 if res == -1:
-    print("Element not found")
+    print("\tElement not found")
 else:
-    print("Element found at index: ", res)
+    print("\tElement found at index: ", res)
 end_time = time.perf_counter_ns()
 execution_time = end_time - start_time
-print("Binary search time on a unsorted duplicated list: ", execution_time)
+print("\tTime: ", execution_time)
+
+print("\nJump search:")
+# Jump search со списком из 10 элементов
+print("Short list:")
+start_time = time.perf_counter_ns()
+res = jump_search(short_list, 7)
+if res == -1:
+    print("\tElement not found")
+else:
+    print("\tElement found at index: ", res)
+end_time = time.perf_counter_ns()
+execution_time = end_time - start_time
+print("\tTime: ", execution_time)
+
+# Jump search со списком из 10_000 элементов
+print("Long list:")
+start_time = time.perf_counter_ns()
+res = jump_search(long_list, 777)
+if res == -1:
+    print("\tElement not found")
+else:
+    print("\tElement found at index: ", res)
+end_time = time.perf_counter_ns()
+execution_time = end_time - start_time
+print("\tTime: ", execution_time)
+
+# Jump search со списком из 10_000_000 элементов
+print("Super long list:")
+start_time = time.perf_counter_ns()
+res = jump_search(big_long_list, 777777)
+if res == -1:
+    print("\tElement not found")
+else:
+    print("\tElement found at index: ", res)
+end_time = time.perf_counter_ns()
+execution_time = end_time - start_time
+print("\tTime: ", execution_time)
+
+# Jump search со списком из 10_000 элементов с повторениями
+print("Duplicated list:")
+start_time = time.perf_counter_ns()
+res = jump_search(duplicated_list, 777)
+if res == -1:
+    print("\tElement not found")
+else:
+    print("\tElement found at index: ", res)
+end_time = time.perf_counter_ns()
+execution_time = end_time - start_time
+print("\tTime: ", execution_time)
+
+# Jump search с неотсортированным списком из 10_000 элементов с повторениями
+print("Unsorted duplicated list:")
+start_time = time.perf_counter_ns()
+res = jump_search(unsorted_list, 777)
+if res == -1:
+    print("\tElement not found")
+else:
+    print("\tElement found at index: ", res)
+end_time = time.perf_counter_ns()
+execution_time = end_time - start_time
+print("\tTime: ", execution_time)
--- Structures/SearchAlgorithms.py
+++ Structures/SearchAlgorithms.py
@@ -1,3 +1,6 @@
+import math
+
+
 def linear_search(lys, element):
     for i in range(len(lys)):
         if lys[i] == element:
@@ -19,3 +22,23 @@ def binary_search(lys, val):
             else:
                 first = mid + 1
     return index
+
+
+def jump_search(lys, val):
+    length = len(lys)
+    jump = int(math.sqrt(length))
+    left, right = 0, 0
+    while left < length and lys[left] <= val:
+        right = min(length - 1, left + jump)
+        if lys[left] <= val <= lys[right]:
+            break
+        left += jump;
+    if left >= length or lys[left] > val:
+        return -1
+    right = min(length - 1, right)
+    i = left
+    while i <= right and lys[i] <= val:
+        if lys[i] == val:
+            return i
+        i += 1
+    return -1
